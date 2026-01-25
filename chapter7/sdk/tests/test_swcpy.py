078f63e19a3ce9d13b22a546f58e3b29e48f196f
NR-Arc <nareshr.official@gmail.com>
2026-01-25 03:00:53 +0000

    chapter7 wrap

--- chapter7/sdk/tests/test_swcpy.py
+++ chapter7/sdk/tests/test_swcpy.py
@@ -31,7 +31,7 @@ def test_list_leagues():
 def test_bulk_player_file_parquet(): 
     """Tests bulk player download through SDK - Parquet"""
 
-    config = SWCConfig(bulk_file_format = "parquet") 
+    config = SWCConfig(swc_base_url="http://0.0.0.0:8000",bulk_file_format = "parquet") 
     client = SWCClient(config)    
 
     player_file_parquet = client.get_bulk_player_file()
